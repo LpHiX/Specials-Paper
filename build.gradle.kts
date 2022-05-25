590d1d507d706269e649b3894788266068a3abed
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2022-05-25 12:45:26 +0800

    Use gradle

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    java
+    `maven-publish`
+    idea
+    eclipse
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+    maven {
+        url = uri("https://papermc.io/repo/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly(libs.paper)
+}
+
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+
+group = "me.lphix"
+version = "1.0-SNAPSHOT"
+description = "Specials"
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
